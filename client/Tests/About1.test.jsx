1ab4566dfe7a8a991e350a90e723ecbbcd678ddf
Nafisa <66S2026@utas.edu.om>
2025-12-14 09:26:12 +0400

    In commit

---
+++ client/Tests/About1.test.jsx
@@ -0,0 +1,5 @@
+import { describe, it, expect } from "vitest";
+import { render,screen } from "@testing-library/react";
+import About from "../src/Components/About";
+import React from "react"; 
+import "@testing-library/jest-dom";  
